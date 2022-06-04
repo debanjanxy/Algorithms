78e5883ac079106288288857800e86da52871841
debanjanxy <programmer.debanjan@gmail.com>
2022-06-05 02:43:41 +0530

    Clean BST array implementation

---
+++ src/main/algorithms/data_structures/binary_tree/bst_static_array.py
@@ -0,0 +1,67 @@
+class BST:
+    def __init__(self, size):
+        self.size = size
+        self.elems = [None] * size
+
+    # Insert a value into the BST
+    def insert(self, node):
+        curr = 0
+        while curr < self.size:
+            if curr < self.size and self.elems[curr] == None:
+                self.elems[curr] = node
+                return 0 
+            if self.elems[curr] > node:
+                curr = 2 * curr + 1
+            elif self.elems[curr] < node:
+                curr = 2 * curr + 2
+            else:
+               print("Duplicate values are not allowed") 
+               return -1
+        print("BST is full")
+
+    # Find a value into the BST
+    def find(self, node):
+        curr = 0
+        while curr < self.size and self.elems[curr]:
+            if self.elems[curr] == node:
+                return curr
+            elif self.elems[curr] > node:
+                curr = 2 * curr + 1
+            else:
+                curr = 2 * curr + 2
+        return -1
+    
+    # Get the successor of a node
+    def successor(self, node):
+        index = self.find(node)
+        if index == -1:
+            raise ValueError("Node does not exist in the tree")
+        left = 2 * index + 1
+        right = 2 * index + 2
+        left_succ, right_succ = None, None
+        while left < self.size and self.elems[left]:
+            left_succ = self.elems[left]
+            left = 2 * left + 2
+        while right < self.size and self.elems[right]:
+            right_succ = self.elems[right]
+            right = 2 * right + 1
+        return left_succ if left_succ else right_succ
+
+    def remove(self, node):
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    bst = BST(20)
+    bst.insert(4)
+    bst.insert(2)
+    bst.insert(1)
+    bst.insert(10)
+    bst.insert(3)
+    bst.insert(6)
+    bst.insert(5)
+    print(bst.elems)
+    print(bst.find(1))
+    print(bst.find(23))
+    print(bst.find(2))
+    print(bst.successor(10))
