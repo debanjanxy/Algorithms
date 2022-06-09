f14c051abbf629364592641d63a070a282b0aede
debanjanxy <programmer.debanjan@gmail.com>
2022-06-10 01:20:34 +0530

    Necessary comments added. Bug-fix to handle duplicate elements in the heap

--- src/main/algorithms/data_structures/priority_queue/binary_heap.py
+++ src/main/algorithms/data_structures/priority_queue/binary_heap.py
@@ -6,22 +6,29 @@ class PQueue:
         self.heap_size = 0
         self.heap_capacity = kwargs.get('capacity', 0)
         self.heap = [None] * self.heap_capacity
+        # Map will be used to map the index of different elements
+        # The value is a list, to handle the duplicate elements
         self.map = defaultdict(list)
         # Costruct the heap by heapifying the input array
         if 'elems' in kwargs:
             self.heap = kwargs.get('elems')
-            self.map = defaultdict(list, {elem: [i] for i, elem in enumerate(self.heap)})
+            for i, elem in enumerate(self.heap):
+                self.map[elem].append(i)
             # Set both the heap size and capacity as the no. of elements
             self.heap_size = self.heap_capacity = len(self.heap)
             # Heapify process: Start from the first non-leaf node
             non_leaf_index = int(self.heap_size/2) - 1
+            # Why start sinking from last non-leaf?
+            # Because in for the last non-leaf it is as simple as a single swap
+            # On top of this we build up the sinking process for each non-leaf node
             for i in range(max(0, non_leaf_index), -1, -1):
                 self.sink(i)
     
     def sink(self, k):
         while True:
             left, right = 2 * k + 1, 2 * k + 2
-            smallest = left # Assume left child is the smallest one
+            # Assume left child is the smallest one
+            smallest = left 
             # If right child is less than left child then right child
             # is the smallest one
             if right < self.heap_size and self.heap[right] < self.heap[left]:
@@ -31,7 +38,6 @@ class PQueue:
             if smallest >= self.heap_size or self.heap[smallest] > self.heap[k]:
                 break
             # swap the nodes (k, smallest)
-            # self.heap[k], self.heap[smallest] = self.heap[smallest], self.heap[k]
             self.swap(k, smallest)
             k = smallest
 
@@ -74,7 +80,10 @@ class PQueue:
         self.heap_size += 1
 
     def swim(self, k):
+        # Get the index of the parent
         parent = int((k - 1) / 2)
+        # If we are not at the root and parent's value
+        # is greater than current node's value then swap
         while k > 0 and self.heap[parent] > self.heap[k]:
             self.swap(parent, k)
             k = parent
@@ -85,6 +94,7 @@ class PQueue:
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
         self.map_swap(i_val, j_val, i, j) 
 
+    # Swap two values and their indices in the map
     def map_swap(self, i_val, j_val, i, j):
         self.map[i_val].remove(i)
         self.map[i_val].append(j)
@@ -110,8 +120,11 @@ class PQueue:
         self.swap(index, self.heap_size - 1)
         self.remove_last()
         parent = int((index - 1) / 2)
+        # If parent is greater than current value
+        # Then swim current value up
         if self.heap[parent] > self.heap[index]:
             self.swim(index)
+        # Else sink it down
         else:
             self.sink(index)
         return removed_data
@@ -139,7 +152,7 @@ class PQueue:
 
 
 if __name__ == "__main__":
-    elems = [4, 1, 2, 5, 3]
+    elems = [9, 10, 4, 5, 1, 2, 2]
     pq = PQueue(elems=elems)
     pq.print_pq()
     pq.add(6)
