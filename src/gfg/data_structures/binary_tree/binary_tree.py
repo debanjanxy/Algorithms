168a63f9621774bfabf944a9a3edf7f28e44734a
debanjanxy <programmer.debanjan@gmail.com>
2022-07-04 23:02:17 +0530

    Deletion of a given node method added

--- src/gfg/data_structures/binary_tree/binary_tree.py
+++ src/gfg/data_structures/binary_tree/binary_tree.py
@@ -24,18 +24,46 @@ def insert_level_order(root, key):
             que.append(curr.left)
             que.append(curr.right)
 
+def deletion(root, key):
+    '''
+    root: root of tree
+    key:  key to be deleted
+    return: root after deleting 
+    '''
+    parent, curr_parent = None, None
+    que = deque([root])
+    while que:
+        curr = que.popleft()
+        if curr.data == key:
+            key_node = curr
+        if curr.left:
+            curr_parent = curr
+            que.append(curr.left)
+        if curr.right:
+            curr_parent = curr
+            que.append(curr.right)
+    if key_node:
+        key_node.data = curr.data
+        if curr_parent.left == curr:
+            curr_parent.left = None
+        elif curr_parent.right == curr:
+            curr_parent.right = None
+        del curr
+    return root
+
 def print_tree(root):
     if not root:
         return
     que = deque([root])
+    result = []
     while que:
         curr = que.popleft()
-        print(curr.data)
+        result.append(curr.data)
         if curr.left:
             que.append(curr.left)
         if curr.right:
             que.append(curr.right)
-    print('-' * 127)
+    print(result)
 
 if __name__ == "__main__":
     root = TreeNode(10)
@@ -46,5 +74,7 @@ if __name__ == "__main__":
     root.right.right = TreeNode(8)
     print_tree(root)
     root = insert_level_order(root, 12)
+    print_tree(root)
+    root = deletion(root, 10)
     print_tree(root)
 
